233af6f91cd951e18e35ebc888b948ddf2ca029b
Semyon <zhigunovsemen@gmail.com>
2024-12-29 10:18:02 +0300

    Добавил операцию присвоения

--- src/main.cc
+++ src/main.cc
@@ -2,14 +2,19 @@
 #include <iostream>
 #include <string_view>
 
-
 class Int : public Object {
 public:
 	~Int() override { delete n_; }
 
-	Int(Int const &i) : n_{new int{*i.n_}} {}
+	Int(Int const & i) : n_{new int{*i.n_}} {}
 
 	Int(int n = 0) : n_{new int{n}} {}
+
+	Int & operator=(Int const & i)
+	{
+		*n_ = *i.n_;
+		return *this;
+	}
 
 	Int const & print(std::ostream & ost = std::cout) const override
 	{
@@ -33,7 +38,7 @@ int main()
 {
 	Int i1;
 	std::cin >> i1;
-	Object * i3 = new Int{i1};
+	Object * i3{new Int{i1}};
 
 	std::cout << i1 << ' ' << i1.id() << ' ' << i1.type() << '\n';
 	std::cout << *i3 << ' ' << i3->id() << ' ' << i3->type() << '\n';
