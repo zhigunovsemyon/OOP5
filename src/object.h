f5a1d8ef4cf793aadc4c8bc41e0fe90041f768ef
Semyon <zhigunovsemen@gmail.com>
2025-01-16 22:32:35 +0300

    Добавил коментов в object.h

--- src/object.h
+++ src/object.h
@@ -21,33 +21,45 @@ public:
 	enum class exceptions {
 		NOT_MATCHING_DERIVED
 	};
+
+	/*Виртуальный деструктор*/
 	virtual ~Object() = default;
 
+	/*Возврат уникального идентификатора*/
 	int id() const { return id_; }
 
+	/*Возврат строки с типом объекта*/
 	virtual std::string_view type() const = 0;
 
+	/*Вывод содержимого в переданный поток*/
 	virtual Object const & print(std::ostream & ost) const = 0;
+
+	/*Чтение из переданного потока*/
 	virtual Object & read(std::istream & ist) = 0;
 
+	/*Сравнение на равенство с другим объектом*/
 	virtual bool equal(Object const &) const = 0;
 
+	/*Сравнение на неравенство с другим объектом*/
 	friend bool operator!=(Object const & a, Object const & b)
 	{
 		return !a.equal(b);
 	}
 
+	/*Сравнение на равенство с другим объектом*/
 	friend bool operator==(Object const & a, Object const & b)
 	{
 		return a.equal(b);
 	}
 
+	/*Чтение содержимого из потока*/
 	friend std::istream & operator>>(std::istream & ist, Object & obj)
 	{
 		obj.read(ist);
 		return ist;
 	}
 
+	/*Вывод содержимого в поток*/
 	friend std::ostream & operator<<(std::ostream & ost, Object const & obj)
 	{
 		obj.print(ost);
@@ -55,10 +67,10 @@ public:
 	}
 
 protected:
+	/*Сокрытый конструктор, инициализирующийся идентификатором*/
 	Object() : id_{count_++} {}
 
 private:
-	static inline int count_;
+	static inline int count_; // Счётчик объектов
 	int const id_; // Идентификатор класса
 };
-
