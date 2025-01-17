c0ffbd784ee392167c832305f911e507945d20f3
Semyon <zhigunovsemen@gmail.com>
2025-01-17 09:30:19 +0300

    Вынес Double уже в cc файл

--- src/Double.h
+++ src/Double.h
@@ -15,48 +15,17 @@ public:
 		return *this;
 	}
 
-	Double const & print(std::ostream & ost) const override
-	{
-		ost << *n_;
-		return *this;
-	}
-
-	Double & add(Object const & other) override
-	{
-		auto cmp{dynamic_cast<Double const *>(&other)};
-		if (nullptr == cmp)
-			throw Object::exceptions::NOT_MATCHING_DERIVED;
-
-		*n_ += *(cmp->n_);
-		return *this;
-	}
-
-	Double & read(std::istream & ist) override
-	{
-		ist >> *n_;
-		return *this;
-	}
-
 	std::string_view type() const override { return "Double"; }
 
-	bool equal(Object const & i) const noexcept(false) override
-	{
-		auto cmp{dynamic_cast<Double const *>(&i)};
-		if (nullptr == cmp)
-			throw Object::exceptions::NOT_MATCHING_DERIVED;
+	Double const & print(std::ostream & ost) const override;
 
-		return *n_ == *(cmp->n_);
-	}
+	Double & add(Object const & other) override;
 
-	int compare(CObject const & other) const noexcept(false) override
-	{
-		auto cmp{dynamic_cast<Double const *>(&other)};
-		if (nullptr == cmp)
-			throw Object::exceptions::NOT_MATCHING_DERIVED;
+	Double & read(std::istream & ist) override;
 
-		auto diff{*n_ - *cmp->n_};
-		return (diff > 0) ? 1 : (diff < 0) ? -1 : 0;
-	}
+	bool equal(Object const & i) const noexcept(false) override;
+
+	int compare(CObject const & other) const noexcept(false) override;
 
 private:
 	double * n_;
